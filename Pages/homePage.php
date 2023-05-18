997d5d0c9d9bc1c5cb7d168df5cf1147e0caf488
Rafael Cerqueira <rafahcerqueira.123@gmail.com>
2023-05-18 20:47:44 -0300

    estilizando pagina home

--- Pages/homePage.php
+++ Pages/homePage.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="../Styles/global.css">
+    <link rel="stylesheet" href="../Styles/home.css">
     <title>Menu</title>
 </head>
 <body>
